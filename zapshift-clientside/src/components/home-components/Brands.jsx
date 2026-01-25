3388682793352166c478aac01e9e70e1447a94d3
AhmadZubayer <ahmadzubayer369@gmail.com>
2026-01-25 11:36:52 +0600

    added brands text

---
+++ zapshift-clientside/src/components/home-components/Brands.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+
+const Brands = () => {
+    return (
+        <div>
+            <div className='text-2'>We've Helped Thousands of Sales Teams</div>
+            
+        </div>
+    );
+};
+
+export default Brands;
--- zapshift-clientside/src/pages/Home.jsx
+++ zapshift-clientside/src/pages/Home.jsx
@@ -3,13 +3,15 @@ import Banner from '../components/home-components/Banner';
 import HowItWorks from '../components/home-components/HowItWorks';
 import '../App.css';
 import OurServices from '../components/home-components/OurServices';
+import Brands from '../components/home-components/Brands';
 
 const Home = () => {
     return (
         <div className='mt-5 gap-y-5 max-w-7xl mx-auto flex flex-col items-center '>
-                <div><Banner></Banner></div>
-                <div><HowItWorks></HowItWorks></div>
-                <div><OurServices></OurServices></div>
+                <Banner></Banner>
+                <HowItWorks></HowItWorks>
+                <OurServices></OurServices>
+                <Brands></Brands>
         </div>
     );
 };
